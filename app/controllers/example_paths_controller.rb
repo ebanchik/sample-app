8ad9e310f09351193a17ae2ae9a1df79c5cc9ec0
ebanchik <ebanchik@gmail.com>
2023-11-30 13:17:27 -0800

    class practice

--- app/controllers/example_paths_controller.rb
+++ app/controllers/example_paths_controller.rb
@@ -3,4 +3,18 @@ class ExamplePathsController < ApplicationController
   def greeting
     render json: {message: 'hello there'}
   end
+  def fortune_teller
+    fortunes = ["You will be an expert coder in the near future", "Struggles lie ahead", "future uncertain"]
+    fortune = fortunes[rand(fortunes.length)]
+    render json: {'Your Fortune': fortune}
+  end
+  def rand_num_gen
+    nums = []
+    6.times do
+      nums << rand(1..60)
+    end
+    render json: {'Lotto Numbers': nums}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/hello", controller: "example_paths", action: "greeting"
+  get "/fortune", controller: "example_paths", action: "fortune_teller"
+  get "rand_number", controller: "example_paths", action: "rand_num_gen"
 end
